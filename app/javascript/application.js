f41928a8d803440d87cc68fd5b21b0ea6c88d933
Fadel ellah ERRAMI <fadelellaherrami@gmail.com>
2024-01-29 23:49:02 +0100

    nothing

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+
+
+
+
+
+
+
+
+
+
+
+
 import "@hotwired/turbo-rails"
+
+
+
 import "controllers"
