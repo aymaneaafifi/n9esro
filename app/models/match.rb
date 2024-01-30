9e2834f467811a8e15d521dcd281fc30dbd4cbac
Fadel ellah ERRAMI <fadelellaherrami@gmail.com>
2024-01-30 19:22:27 +0100

    ba9i massalitch les models

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,16 @@
+
+
+
+
+
+
+
 class Match < ApplicationRecord
   belongs_to :terrain
   belongs_to :user
+  has_many :teams, dependent: :destroy
+
+  validates :user_id, uniqueness: { scope: :date, message: "et utilisateur doivent être uniques simultanément" }
+  validates :title, length: { minimum: 3, maximum: 12, too_short: "short title", too_long: "long title" }
+  validates :description, length: { minimum: 10, too_short: "short description" }
 end
