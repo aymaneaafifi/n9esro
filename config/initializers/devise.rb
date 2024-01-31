52fec218ee075284a4ee5ae2aa2c047bfc06f071
Essoufi-oussama <87605323+Essoufi-oussama@users.noreply.github.com>
2024-02-01 00:02:36 +0100

    configured .env file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, '61896173443-aikq6fvjfl1e93bsi4sd1dejoepanbbl.apps.googleusercontent.com', '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'
+  config.omniauth :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
