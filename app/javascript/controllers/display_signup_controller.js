0a78cd4aff3c76243796f4366fa0fb031590e784
Essoufi-oussama <87605323+Essoufi-oussama@users.noreply.github.com>
2024-02-01 00:41:38 +0100

    added javascript to sign in and signup so now it works in same page

---
+++ app/javascript/controllers/display_signup_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["signUpArea", "signInArea", "signUpImage", "signInImage"];
+
+  toggleSignup(event) {
+    event.preventDefault();
+    this.signUpAreaTarget.classList.toggle("d-none");
+    this.signUpImageTarget.classList.toggle("d-none");
+    this.signInAreaTarget.classList.toggle("d-none");
+    this.signInImageTarget.classList.toggle("d-none");
+  }
+}
