46461662d1aee1432bc781f448210778f1525752
Essoufi-oussama <87605323+Essoufi-oussama@users.noreply.github.com>
2024-02-06 00:01:47 +0100

    fixed problem of a css interfering with other css, added custom path after sign up and another after sign in if user doesnt have photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :bio, :photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :bio, :photo])
   end
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User) && resource.photo.key.nil?
+      complete_information_path
+    else
+      super
+    end
+  end
 end
