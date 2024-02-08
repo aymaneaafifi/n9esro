d1df3287f5fcc3b837fcd918c646b97b1519435b
Essoufi-oussama <87605323+Essoufi-oussama@users.noreply.github.com>
2024-02-08 16:08:51 +0100

    done

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true # @2.11.8
 pin "flatpickr" # @4.6.13
+pin "fullcalendar" # @6.1.10
---
+++ vendor/javascript/fullcalendar.js
@@ -0,0 +1,2 @@
+import{globalPlugins as r}from"@fullcalendar/core/index.js";export*from"@fullcalendar/core/index.js";import l from"@fullcalendar/interaction/index.js";export*from"@fullcalendar/interaction/index.js";import e from"@fullcalendar/daygrid/index.js";import i from"@fullcalendar/timegrid/index.js";import n from"@fullcalendar/list/index.js";import o from"@fullcalendar/multimonth/index.js";r.push(l,e,i,n,o);
+
